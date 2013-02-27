3f4ea1f709cb500c755157506d30def3dc787109
Elijah Wright <elijah.wright@gmail.com>
2013-02-27 16:59:10 -0600

    Add notes about how to modify a debian installer CD to use preseed.

--- build-debian-stable-x86-vm.sh
+++ build-debian-stable-x86-vm.sh
@@ -16,6 +16,28 @@ curl -k https://github.com/elijah/smartos-debian-preseed/debian-stable-x86.cfg/r
 # TODO - we ought to be putting the preseed file into our .iso of the OS, so that it can boot and install totally unattended.
 #        For now, though, we'll suffer through using vnc to intercept GRUB / syslinux and start the install.
 
+# OK, so looking at the files on a debian ISO, here's what we have to do:
+# 1) download the iso
+# 2) loopback mount it
+# 3) inject the preseed file into the ROOT (/) of the ISO
+# 4) update $ROOT/isolinux/txt.cfg so that we have the right boot arguments to use preseed with the installer.
+# 4b) Those arguments look like this:
+#     default vmlinuz
+#     append preseed/file=/hd-media/preseed.cfg locale=en_US console-keymaps-at/keymap=us languagechooser/language-name=English countrychooser/shortlist=US vga=normal initrd=initrd.gz  --
+#
+#     Whereas a default txt.cfg looks like this:
+#
+#     default install
+#     label install
+#	menu label ^Install
+#	menu default
+#	kernel /install.386/vmlinuz
+#	append vga=788 initrd=/install.386/initrd.gz -- quiet 
+#
+#
+#
+
+
  
 vmadm boot $UUID order=cd,once=d cdrom=$CDROM,cdrom=$TOOLS 
 
